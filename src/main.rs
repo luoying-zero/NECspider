ad6b81be4df7e9e36be0ba2cb481564520eec4fc
luoying-zero <105333299+luoying-zero@users.noreply.github.com>
2025-03-11 23:25:07 +0800

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn main() {
     let max_concurrent = 2;
     let ids: Vec<u64> = (1..=10).into_iter().collect();
     let mut join_set = JoinSet::new();
-    
+
     for id in ids {
         while join_set.len() >= max_concurrent {
             join_set.join_next().await.unwrap().unwrap();
@@ -20,28 +20,30 @@ async fn main() {
             let res = reqwest::Client::new()
                 .get(format!("https://music.163.com/playlist?id={}", id))
                 .send()
-                .await?.text().await?;
+                .await?
+                .text()
+                .await?;
             let select = scraper::Selector::parse("div.user > span.name > a").unwrap();
-            if let Some(name) = scraper::Html::parse_document(&res).select(&select).next(){
-                if name.value().name() == "PurionPurion"{
+            if let Some(name) = scraper::Html::parse_document(&res).select(&select).next() {
+                if name.value().name() == "PurionPurion" {
                     println!("{:?}", id);
                 }
             }
         });
     }
-    
+
     println!("DONE SPAWNING");
-    
+
     while let Some(output) = join_set.join_next().await {
         output.unwrap();
     }
-    
+
     println!("ALL DONE");
 }
 
 // async fn my_bg_task(id: u64) {
-    // let num: u64 = thread_rng().gen_range(10..200);
-    // println!("START id: {} with {}ms", id, num);
-    // sleep(Duration::from_millis(num)).await;
-    // println!("STOP id: {}", id);
+// let num: u64 = thread_rng().gen_range(10..200);
+// println!("START id: {} with {}ms", id, num);
+// sleep(Duration::from_millis(num)).await;
+// println!("STOP id: {}", id);
 // }
