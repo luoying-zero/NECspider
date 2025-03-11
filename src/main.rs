c2a6106bef084d5d7cba707aedd399fb5abd1c01
luoying-zero <105333299+luoying-zero@users.noreply.github.com>
2025-03-11 16:49:31 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use spider::tokio;
+use spider::website::Website;
+use tokio::io::AsyncWriteExt;
+
+#[tokio::main]
+async fn main() {
+    let mut website: Website = Website::new("https://music.163.com/playlist?id=422276169");
+    let mut rx2 = website.subscribe(16).unwrap();
+
+    tokio::spawn(async move {
+        let mut stdout = tokio::io::stdout();
+
+        while let Ok(res) = rx2.recv().await {
+            let _ = stdout
+                .write_all(format!("- {}\n", res.get_url()).as_bytes())
+                .await;
+        }
+    });
+
+    website.crawl().await;
 }
