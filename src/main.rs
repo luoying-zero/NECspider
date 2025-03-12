2fc2723b8cd4503e29ce680b89e36b5529afc35e
luoying-zero <105333299+luoying-zero@users.noreply.github.com>
2025-03-13 00:48:00 +0800

    Add

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use reqwest;
 use scraper;
-use std::future::Future;
-use std::time::Duration;
 use std::env::args;
+// use std::future::Future;
+// use std::time::Duration;
 use tokio;
-//use tokio::time::{sleep, Duration};
 use tokio::task::JoinSet;
-//use rand;
+use backon::ExponentialBuilder;
+use backon::Retryable;
 
 #[tokio::main]
 async fn main() {
@@ -21,9 +21,20 @@ async fn main() {
         while join_set.len() >= max_concurrent {
             join_set.join_next().await.unwrap().unwrap();
         }
-        join_set.spawn(retry_on_err(
-            move || find_Author(id)
-        ));
+        join_set.spawn(move || async {
+            let res = reqwest::get(format!("https://music.163.com/playlist?id={}", id))
+                .await?
+                .text()
+                .await
+                .unwrap();
+            let select = scraper::Selector::parse("div.user > span.name > a").unwrap();
+            let html = scraper::Html::parse_document(&res);
+            if let Some(name) = html.select(&select).next() {
+                if name.value().name() == "PurionPurion" {
+                    println!("{:?}", id);
+            }}
+            Ok(())
+        });
     }
 
     println!("DONE SPAWNING");
@@ -35,52 +46,57 @@ async fn main() {
     println!("ALL DONE");
 }
 
-async fn find_Author(id: u64) -> Result<(), reqwest::Error> {
-    let res = reqwest::get(format!("https://music.163.com/playlist?id={}", id)).await?.text().await.unwrap();
-    let select = scraper::Selector::parse("div.user > span.name > a").unwrap();
-    let html = scraper::Html::parse_document(&res);
-    let name = html.select(&select).next().unwrap();
-    if name.value().name() == "PurionPurion" {
-        println!("{:?}", id);
-    }
-    Ok(())
-}
+// async fn find_Author(id: u64) -> Result<(), reqwest::Error> {
+    // let res = reqwest::get(format!("https://music.163.com/playlist?id={}", id))
+        // .await?
+        // .text()
+        // .await
+        // .unwrap();
+    // let select = scraper::Selector::parse("div.user > span.name > a").unwrap();
+    // let html = scraper::Html::parse_document(&res);
+    // if let Some(name) = html.select(&select).next() {
+        // if name.value().name() == "PurionPurion" {
+            // println!("{:?}", id);
+    // }}
+    // Ok(())
+// }
 
-async fn retry_on_err<T, E, F, Fut>(f: F)
-where
-    E: std::fmt::Debug,
-    F: Fn() -> Fut,
-    Fut: Future<Output = Result<T, E>>,
-{
-    let maxtry = 5;
-    //let now = Instant::now();
-    let backoff = Duration::from_millis(500);
-    //let factor = 1.5;
-    //let limit = Duration::from_secs(60 * 2);
-    //let warn = Duration::from_secs(60 * 60);
-    //let mut rng = rand::rngs::OsRng;
-    //let mut jitter = || rng.gen_range(Duration::ZERO..backoff);
+// async fn retry_on_err<T, E, F, Fut>(f: F)
+// where
+    // E: std::fmt::Debug,
+    // F: Fn() -> Fut,
+    // Fut: Future<Output = Result<T, E>>,
+// {
+    // let maxtry = 5;
+    // //let now = Instant::now();
+    // let backoff = Duration::from_millis(500);
+    // //let factor = 1.5;
+    // //let limit = Duration::from_secs(60 * 2);
+    // //let warn = Duration::from_secs(60 * 60);
+    // //let mut rng = rand::rngs::OsRng;
+    // //let mut jitter = || rng.gen_range(Duration::ZERO..backoff);
 
-    for i in 0..=maxtry {
-        match f().await {
-            Ok(_) => {
-                break;
-            }
-            Err(e) => {
-                //let elapsed = now.elapsed();
-                //if elapsed > warn {
-                //let elapsed = humantime::format_duration(elapsed);
-                //error!(%elapsed);
-                //}
-                //let retry_in = backoff.mul_f32(factor).min(limit) + jitter();
-                if i == maxtry{
-                    println!("{:?}", e);
-                }
-                tokio::time::sleep(backoff).await;
-            }
-        }
-    }
-}
+    // for i in 0..=maxtry {
+        // match f().await {
+            // Ok(_) => {
+                // break;
+            // }
+            // Err(e) => {
+                // //let elapsed = now.elapsed();
+                // //if elapsed > warn {
+                // //let elapsed = humantime::format_duration(elapsed);
+                // //error!(%elapsed);
+                // //}
+                // //let retry_in = backoff.mul_f32(factor).min(limit) + jitter();
+                // if i == maxtry {
+                    // println!("{:?}", e);
+                // }
+                // tokio::time::sleep(backoff).await;
+            // }
+        // }
+    // }
+// }
+
 // async fn my_bg_task(id: u64) {
 // let num: u64 = thread_rng().gen_range(10..200);
 // println!("START id: {} with {}ms", id, num);
