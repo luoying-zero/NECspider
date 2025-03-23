4bc9ee388ff0bd07eb477631429d1b83b9d73d27
luoying-zero <3348015570@qq.com>
2025-03-23 17:20:13 +0800

    用信号取代joinset来处理结果

--- src/main-static.rs
+++ src/main-static.rs
@@ -1,118 +1,130 @@
 use backon::ConstantBuilder;
 use backon::Retryable;
+use bytes::Bytes;
+use indicatif::ProgressBar;
+use indicatif::ProgressDrawTarget;
+use indicatif::ProgressStyle;
 use reqwest;
+//use scraper;
+use std::collections::HashMap;
 use std::env::args;
-use std::sync::Arc;
 use std::time::Duration;
 // use std::future::Future;
 // use std::time::Duration;
-use scraper;
 use tokio;
 use tokio::task::JoinSet;
 
 #[tokio::main]
 async fn main() {
+    let filed = Bytes::from_static(b"\"userId\":");
+    let author = Bytes::from_static(b"62696289,");
     let mut arguments = args();
     let max_concurrent = arguments.nth(1).unwrap().parse::<usize>().unwrap();
     let begin = arguments.next().unwrap().parse::<u64>().unwrap();
     let end = arguments.next().unwrap().parse::<u64>().unwrap();
-    let mut join_set: JoinSet<Result<(), reqwest::Error>> = JoinSet::new();
-    let semaphore = Arc::new(tokio::sync::Semaphore::new(max_concurrent));
-    let client = reqwest::Client::new();
+    let mut join_set: JoinSet<Result<Option<u64>, String>> = JoinSet::new();
+    let bar = ProgressBar::new(end - begin);
+    bar.set_style(
+        ProgressStyle::with_template("{bar:40} {pos:>7}/{len:7} | {elapsed}/{eta} | {per_sec}")
+            .unwrap(),
+    );
+    bar.set_draw_target(ProgressDrawTarget::stderr_with_hz(1));
+
+    let client = reqwest::Client::builder()
+        .http1_only()
+        .timeout(Duration::from_secs(50))
+        .pool_idle_timeout(Duration::from_secs(72))
+        .pool_max_idle_per_host(usize::MAX)
+        .gzip(true)
+        .brotli(true)
+        .zstd(true)
+        .deflate(true)
+        .build()
+        .unwrap();
 
     for id in begin..end {
-        // while join_set.len() >= max_concurrent {
-            // join_set.join_next().await.unwrap().unwrap();
-        // }
-        let client_clone = client.clone();
-        let permit = semaphore.clone().acquire_owned().await.unwrap();
-        join_set.spawn(async move {
-            let req = move || {
-                client_clone
-                    .get(format!("https://music.163.com/playlist?id={}", id))
-                    .send()
-            };
-            let res = req
-                .retry(ConstantBuilder::default().with_delay(Duration::from_millis(0)))
-                .await?
-                .text()
-                .await
-                .unwrap();
-            drop(permit);
-            let select = scraper::Selector::parse("div.user > span.name > a").unwrap();
-            let html = scraper::Html::parse_document(&res);
-            if let Some(name) = html.select(&select).next() {
-                if name.value().name() == "PurionPurion" {
-                    println!("{:?}", id);
-                }
+        while join_set.len() >= max_concurrent {
+            let res = join_set.join_next().await.unwrap();
+            match res {
+                Ok(Ok(Some(id))) => println!("\"https://music.lliiiill.com/playlist/{id}\","),
+                Ok(Ok(None)) => (),
+                Ok(Err(e)) => eprintln!("{e}"),
+                Err(err) => eprintln!("Join Error: {err:#?}"),
             }
-            Ok(())
+        }
+        let filed = filed.clone();
+        let author = author.clone();
+        let client_clone = client.clone();
+        // if (id - begin) % ((end - begin) / 100) == 0 {
+        // bar.inc((end - begin) / 100);
+        // }
+        bar.inc(1);
+        join_set.spawn(async move {
+            let mut params = HashMap::new();
+            params.insert("id", format!("{id}"));
+            let req = || async {
+                let bytes = client_clone
+                    .post("http://music.163.com/api/v6/playlist/detail")
+                    .form(&params)
+                    .send()
+                    .await?
+                    .bytes()
+                    .await?;
+                Ok::<bytes::Bytes, reqwest::Error>(bytes)
+            };
+            let res = match req
+                .retry(ConstantBuilder::default()
+                    .with_delay(Duration::from_millis(0))
+                    .with_max_times(10)
+                )
+                .await
+            {
+                Ok(bytes) => bytes,
+                Err(e) => return Err(format!("Err pid {id} {e:#?}")),
+            };
+            match check_bytes_sequence(res, filed, author) {
+                true => Ok(Some(id)),
+                _ => Ok(None),
+            }
         });
     }
 
-    println!("DONE SPAWNING");
+    eprintln!("{:#?}, {}", bar.duration(), bar.per_sec());
+    bar.finish();
 
-    while let Some(output) = join_set.join_next().await {
-        output.unwrap();
+    //let mut output = Vec::new();
+    while let Some(res) = join_set.join_next().await {
+        match res {
+            Ok(Ok(Some(id))) => println!("\"https://music.lliiiill.com/playlist/{id}\","),
+            Ok(Ok(None)) => (),
+            Ok(Err(e)) => eprintln!("{e}"),
+            Err(err) => eprintln!("Join Error: {err:#?}"),
+        }
     }
-
-    println!("ALL DONE");
 }
 
-// async fn find_Author(id: u64) -> Result<(), reqwest::Error> {
-// let res = reqwest::get(format!("https://music.163.com/playlist?id={}", id))
-// .await?
-// .text()
-// .await
-// .unwrap();
+pub fn check_bytes_sequence(haystack: Bytes, needle1: Bytes, needle2: Bytes) -> bool {
+    // 查找第一个子序列的位置
+    if let Some(pos) = find_subsequence(&haystack, &needle1) {
+        // 检查剩余部分是否以第二个子序列开头
+        let remaining = &haystack[pos + needle1.len()..];
+        remaining.starts_with(&needle2)
+    } else {
+        false
+    }
+}
+
+/// 辅助函数：在字节切片中查找子序列的位置。
+fn find_subsequence(haystack: &Bytes, needle: &Bytes) -> Option<usize> {
+    haystack
+        .windows(needle.len())
+        .position(|window| window == needle)
+}
+
 // let select = scraper::Selector::parse("div.user > span.name > a").unwrap();
 // let html = scraper::Html::parse_document(&res);
 // if let Some(name) = html.select(&select).next() {
 // if name.value().name() == "PurionPurion" {
 // println!("{:?}", id);
-// }}
-// Ok(())
 // }
-
-// async fn retry_on_err<T, E, F, Fut>(f: F)
-// where
-// E: std::fmt::Debug,
-// F: Fn() -> Fut,
-// Fut: Future<Output = Result<T, E>>,
-// {
-// let maxtry = 5;
-// //let now = Instant::now();
-// let backoff = Duration::from_millis(500);
-// //let factor = 1.5;
-// //let limit = Duration::from_secs(60 * 2);
-// //let warn = Duration::from_secs(60 * 60);
-// //let mut rng = rand::rngs::OsRng;
-// //let mut jitter = || rng.gen_range(Duration::ZERO..backoff);
-
-// for i in 0..=maxtry {
-// match f().await {
-// Ok(_) => {
-// break;
-// }
-// Err(e) => {
-// //let elapsed = now.elapsed();
-// //if elapsed > warn {
-// //let elapsed = humantime::format_duration(elapsed);
-// //error!(%elapsed);
-// //}
-// //let retry_in = backoff.mul_f32(factor).min(limit) + jitter();
-// if i == maxtry {
-// println!("{:?}", e);
-// }
-// tokio::time::sleep(backoff).await;
-// }
-// }
-// }
-// }
-
-// async fn my_bg_task(id: u64) {
-// let num: u64 = thread_rng().gen_range(10..200);
-// println!("START id: {} with {}ms", id, num);
-// sleep(Duration::from_millis(num)).await;
-// println!("STOP id: {}", id);
 // }
